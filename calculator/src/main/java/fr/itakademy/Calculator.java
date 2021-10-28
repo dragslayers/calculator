6511910b68b3b9379f2e3dcfa2e8813d1a74a5fc
mzhang <m.zhang@it-students.fr>
2021-10-28 14:25:32 +0200

    ajout des tests

--- calculator/src/main/java/fr/itakademy/Calculator.java
+++ calculator/src/main/java/fr/itakademy/Calculator.java
@@ -11,7 +11,7 @@ class Calculator {
     }
 
     public int multiplication(int a, int b) {
-        return a + b;
+        return a * b;
     }
 
     public double division(double a, double b) {
--- calculator/src/test/java/fr/itakademy/CalculatorTest.java
+++ calculator/src/test/java/fr/itakademy/CalculatorTest.java
@@ -1,9 +1,9 @@
 package fr.itakademy;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
     private final Calculator calculator = new Calculator();
@@ -14,7 +14,56 @@ public class CalculatorTest {
     }
 
     @Test
+    void should_substract_correctly() {
+        assertEquals(0, calculator.soustraction(4, 4));
+    }
+
+    @Test
+    void should_multiply_correctly() {
+        assertEquals(16, calculator.multiplication(4, 4));
+    }
+
+    @Test
+    void should_divide_correctly() {
+        assertEquals(1, calculator.division(4, 4));
+    }
+
+    @Test
+    void should_power_correctly() {
+        assertEquals(64, calculator.puissance(4,3));
+    }
+
+    @Test
+    void should_racine_correctly() {
+        assertEquals(2, calculator.racine(4));
+    }
+
+    @Test
     void should_not_add_correctly() {
         assertNotEquals(12, calculator.addition(5, 8));
+    }
+
+    @Test
+    void should_not_substract_correctly() {
+        assertNotEquals(1, calculator.soustraction(4, 4));
+    }
+
+    @Test
+    void should_not_multiply_correctly() {
+        assertNotEquals(26, calculator.multiplication(4, 4));
+    }
+
+    @Test
+    void should_not_divide_correctly() {
+        assertNotEquals(6, calculator.division(4, 4));
+    }
+
+    @Test
+    void should_not_power_correctly() {
+        assertNotEquals(6, calculator.puissance(4, 3));
+    }
+    @Test
+    void should_not_square_correctly() {
+        assertNotEquals(6, calculator.racine(4));
     }
 }
