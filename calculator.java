a36afaab0eb6bbd885effe522b6edbc9221e0853
mzhang <m.zhang@it-students.fr>
2021-10-28 10:46:41 +0200

    racine

--- calculator.java
+++ calculator.java
@@ -23,9 +23,20 @@ class calculator {
         return a * puissance(a, b - 1);
     }
 
+    public static double racine(int a) {
+        double tmp;
+        double d = a / 2;
+        do {
+            tmp = d;
+            d = (tmp + (a / tmp)) / 2;
+        } while ((tmp - d) != 0);
+        return d;
+    }
+
     public static void main(String[] args) {
         double x = division(5.0, 2.0);
         int y = puissance(3, 3);
-        System.out.println(y);
+        double z = racine(2);
+        System.out.println(z);
     }
 }
