ed552532cb2728996fd5dcafbc7cc7fc315024fb
mzhang <m.zhang@it-students.fr>
2021-10-28 10:14:59 +0200

    initial commit

---
+++ calculator.java
@@ -0,0 +1,22 @@
+class calculator {
+
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public int soustraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a + b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
