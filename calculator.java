0596024bc275f71074447c568a3df541abaae353
mzhang <m.zhang@it-students.fr>
2021-10-28 10:25:52 +0200

    puissance

--- calculator.java
+++ calculator.java
@@ -16,6 +16,13 @@ class calculator {
         return a / b;
     }
 
+    public static int puissance(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return a * puissance(a, b--);
+    }
+
     public static void main(String[] args) {
         double x = division(5.0, 2.0);
         System.out.println(x);
